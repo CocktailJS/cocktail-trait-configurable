10fd83baf1da2eaab5beed75ec88333f83f35979
Maximiliano Fierro <elmasse@gmail.com>
2013-11-08 20:58:53 -0300

    Configurable Trait. Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js'],
+        options: {
+            es5: true,
+            expr: true,
+            globals: {describe: true, it: true, expect: true, define: true, beforeEach: true, afterEach: true}
+        }
+      }
+    },
+
+    simplemocha: {
+      unit: {
+          src: 'test/unit/**/*.js',
+          options: {
+              timeout: 3000,
+              ignoreLeaks: false,
+              ui: 'bdd',
+              reporter: 'spec'
+          }
+      },
+      integration: {
+          src: 'test/integration/**/*.js',
+          options: {
+              timeout: 3000,
+              ignoreLeaks: false,
+              ui: 'bdd',
+              reporter: 'spec'
+          }
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'test']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'test']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  // Default task.
+  grunt.registerTask('test', ['simplemocha:unit','simplemocha:integration']);
+
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ test/unit/Configurable.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var chai = require("chai"),
+    // sinon = require("sinon"),
+    sinonChai = require("sinon-chai"),
+    expect = chai.expect,
+    cocktail = require('cocktail'),
+    Configurable = require('../../lib/Configurable');
+
+chai.use(sinonChai);
+
+describe('Configurable Trait Unit Test', function(){
+
+    describe('Configurable added as Trait on ConfiguredClass', function(){
+        var ConfiguredClass;
+
+        ConfiguredClass = cocktail.mix({
+            '@as': 'class',
+            '@traits': [Configurable]
+        });
+        
+        it('should provide `configurable` method on host class', function(){
+            var sut = new ConfiguredClass();
+            expect(sut).to.respondTo('configure');
+        });
+    });
+
+    describe('Configurable added as Trait on ConfiguredClass with property `configured`', function(){
+        var ConfiguredClass,
+            privateValue = "PRIVATE";
+
+        ConfiguredClass = cocktail.mix({
+            '@as': 'class',
+            '@traits': [Configurable],
+            '@properties': {
+                configured: false,
+                value: undefined
+            },
+
+            privateValue: privateValue
+        });
+        
+        it('should call setConfigured method with boolean options.configured value', function(){
+            var sut = new ConfiguredClass(),
+                options = {};
+
+            //make it true
+            options.configured = true;
+            sut.configure(options);
+            expect(sut.isConfigured()).to.be.equal(options.configured);
+
+            //make it false
+            options.configured = false;
+            sut.configure(options);            
+            expect(sut.isConfigured()).to.be.equal(options.configured);         
+        });
+
+        it('should call setValue method with any options.value value', function(){
+            var sut = new ConfiguredClass(),
+                options = {};
+
+            //make it object
+            options.value = {'some': 1};
+            sut.configure(options);
+            expect(sut.getValue()).to.be.equal(options.value);
+
+            //make it null
+            options.value = null;
+            sut.configure(options);
+            expect(sut.getValue()).to.be.equal(options.value);
+
+            //make it 0
+            options.value = 0;
+            sut.configure(options);
+            expect(sut.getValue()).to.be.equal(options.value);
+
+            //make it String
+            options.value = "string";
+            sut.configure(options);
+            expect(sut.getValue()).to.be.equal(options.value);
+
+            //make it undefined
+            options.value = undefined;
+            sut.configure(options);
+            expect(sut.getValue()).to.be.equal(options.value);
+
+        });
+
+        it('should not modify values with no setters', function(){
+            var sut = new ConfiguredClass(),
+                options = {};
+
+            options.privateValue = "SHOULD BE DISCARDED";
+            sut.configure(options);
+            expect(sut.privateValue).to.be.equal(privateValue);
+
+        });
+
+        it('should not add values with no setters', function(){
+            var sut = new ConfiguredClass(),
+                options = {};
+
+            options.unknown = "UNKNOWN";
+            sut.configure(options);
+            expect(sut.unknown).to.be.equal(undefined);
+
+        });
+
+
+    });
+
+
+
+});
